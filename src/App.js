50a758b0ee2853189763df8630f5e611f86a8b63
= <stevenabaco@gmail.com>
2021-02-02 22:09:03 -0600

    Animate React logo to pop up and fade in

--- src/App.js
+++ src/App.js
@@ -1,14 +1,56 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { useRef, useEffect } from "react";
+import logo from "./logo.svg";
+import { TweenMax, Power3 } from "gsap";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Component1/>
-      </div>
-    );
-  }
+function App() {
+  let logoItem = useRef(null);
+  let textItem = useRef(null);
+
+	useEffect(() => {
+		TweenMax.to(logoItem, 0.8, {
+		opacity: 1,
+		y: -20,
+		ease: Power3.easeOut,
+    }
+  )
+		TweenMax.to(textItem, 0.8, {
+		opacity: 1,
+		y: -20,
+      ease: Power3.easeOut,
+      delay: .5
+    }
+  )
+  
+  }, []);
+
+	console.log(logoItem);
+	return (
+    <div className="App">
+      <balls />
+			<header className="App-header" alt="logo">
+				<img
+					ref={(el) => {
+						logoItem = el;
+					}}
+					src={logo}
+					className="App-Logo"
+					alt="logo"
+				/>
+				<p ref={el => {textItem = el}}>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://react.js.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/balls.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const balls = () => {
+    return (
+        <div className="circle-container">
+        <div className="circle"></div>    
+        <div className="circle"></div>    
+        <div className="circle"></div>    
+        </div>
+    )
+     
+    }
+
+export default balls
+
